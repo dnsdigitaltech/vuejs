11cdd11254412f5eae68ef77e6ad5a0cb28c11ec
Davi Bernardo <dnssites@gmail.com>
2020-12-08 19:32:25 +0000

    Analise e alteracao de um projeto Vue Cli

---
+++ vuecli001/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <p>Este é o componente App</p>
+    <novo />
+  </div>
+</template>
+
+<script>
+import Novo from './components/Novo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Novo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
