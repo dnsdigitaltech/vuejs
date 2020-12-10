6d01249b05cf551bf6d1f0df4c7e4d97074cbc90
Davi Bernardo <dnssites@gmail.com>
2020-12-10 16:02:17 -0300

    Registrar components local

--- vuecli001/src/App.vue
+++ vuecli001/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <h3>título</h3>
-    <div class="alert alert-success text-center">Mensagem</div>
+    <h3>App component</h3>
+    <app-servidor></app-servidor>
   </div>
 </template>
 
 <script>
-  
+
+import Servidor from './Servidor.vue';
+export default{
+  components: {
+    'app-servidor': Servidor
+  }
+}
 </script>
 
 <style>
