59c81ec83d46a89ada4a0f7487109a0b562f52db
Davi Bernardo <dnssites@gmail.com>
2020-12-10 18:02:07 -0300

    Exercicio 2 - comunicaçao de pararent para child

--- vuecli001/src/App.vue
+++ vuecli001/src/App.vue
@@ -1,39 +1,27 @@
 <template>
   <div class="container mt-3 box">
-    <button @click="clickNome">Nome</button>
-    <button @click="clickApelido">Apelido</button>
-    <!--<p>{{nome}} {{apelido}}</p>-->
-    <app-infopessoa 
-    :infonome="nome" 
-    :infoapelido="apelido">
-    </app-infopessoa>
+    <button @click="executar(0)" class="btn btn-primary m-1">Click 0</button>
+    <button @click="executar(100)" class="btn btn-primary m-1">Click 100</button>
+    <button @click="executar(200)" class="btn btn-primary m-1">Click 200</button>
+    <app-componente :meuvalor="valor"></app-componente>
   </div>
 </template>
 
 <script>
-import infopessoa from './components/infopessoa';
-
+  import componente from  './components/componente';
   export default{
     components:{
-      'app-infopessoa':infopessoa
+        'app-componente': componente
     },
     data() {
       return {
-        nome: '',
-        apelido: ''
+        valor: 0
       }
     },
     methods: {
-      clickNome(){
-        this.nome = 'Davi'
-      },
-      clickApelido(){
-        this.apelido = 'Bernardo'
+      executar(value){
+        this.valor = value
       }
     },
   }
 </script>
-
-<style scoped>
-
-</style>
